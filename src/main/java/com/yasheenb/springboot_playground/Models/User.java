cb8a573c0a5b379feeebcc1c8900593f9445d700
YasheenB <yasheenb@icloud.com>
2025-12-22 12:38:59 +0200

    Updated the table name using the annotation @Table(name="\"User\"") due to this being a keyword.

--- src/main/java/com/yasheenb/springboot_playground/Models/User.java
+++ src/main/java/com/yasheenb/springboot_playground/Models/User.java
@@ -1,11 +1,9 @@
 package com.yasheenb.springboot_playground.Models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "\"User\"") // user is a keyword in H2.
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
