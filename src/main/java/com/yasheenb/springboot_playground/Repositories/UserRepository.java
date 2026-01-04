168d4085d6f789662b9ee7a1bb335580b3992e34
YasheenB <yasheenb@icloud.com>
2026-01-04 18:21:42 +0200

    Cleaning up the branch by removing files not related to the learning path.

--- src/main/java/com/yasheenb/springboot_playground/Repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yasheenb.springboot_playground.Repositories;
-
-import com.yasheenb.springboot_playground.Models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
